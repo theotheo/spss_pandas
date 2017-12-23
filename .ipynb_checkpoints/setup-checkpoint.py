0e908b1f1b2c6046f1f429ad51753257ca9eed8d
Ildar Belyalov <ibelyalov@yandex.ru>
2017-12-23 13:56:20 +0000

    Implement a package

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spss_pandas',
+      version='0.0.1',
+      description='SPSS to Pandas',
+      author='I',
+      author_email='i@example.com',
+      license='MIT',
+      packages=['spss_pandas'],
+      zip_safe=True
+     )
